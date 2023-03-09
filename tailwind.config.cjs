0d0ea34243876ea5eb88eccded7ca1f92a824774
IGotYB <igotyb.stream@gmail.com>
2023-03-09 12:18:45 +0100

    setup tailwind confing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,44 +22,20 @@ theme: {
       // => @media (min-width: 1536px) { ... }
   },
   fontSize: {
-      sm: "0.8rem",
-      base: "1rem",
-      xl: "1.25rem",
-      xxl: "2rem",
+      sm: "1rem",
+      md: "1.5rem",
+      lg: "2rem",
   },
   fontFamily: {
-      logoFont: ["Omegle", "sans-serif"],
-      sloganFontRegular: ["RobotoMonoRegular", "monospace"],
-      sloganFontThin: ["RobotoMonoThin", "monospace"],
-      sloganFontBold: ["RobotoMonoBold", "monospace"],
+      mont: ["Montserrat", "regular"],
   },
 
   extend: {
       colors: {
-          primary: "#FF4252",
-          primaryVariant: "#FF6F51",
-          secondary: "#14375A",
-          secondaryVariant: "#5C7792",
-          tertiary: "#6BBEB9",
-          coolBlack: "#3A3C3D",
-      },
-      borderRadius: {
-          default: "4px",
-          large: "12px",
-          xl_large: "32px"
-
-      },
-      padding: {
-          sm: "8px",
-          md: "16px",
-          lg: "24px",
-          xl: "48px",
-      },
-      spacing: {
-          sm: "8px",
-          md: "16px",
-          lg: "24px",
-          xl: "48px",
+          primary: "#252D48",
+          secondary: "#Secondary",
+          wrong: "#FF5D5D",
+          neutral: "#FFFFFF",
       },
       maxWidth: {
           1: "100%",
@@ -68,8 +44,6 @@ theme: {
           "1/4": "25%",
           "1/5": "20%",
           "1/3": "30%",
-
-
       },
       maxHeight: {
           1: "100%",

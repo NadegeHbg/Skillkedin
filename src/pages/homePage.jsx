9da96589b898145da34c95e42db1200c479270a5
igotyb <igotyb.stream@gmail.com>
2023-03-10 23:16:42 +0100

    preparation of categories

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -16,41 +16,49 @@ const HomePage = () => {
         // console.log(inputSearch)
     }
 
+    const categories = async () => {
+        const response = await fetch()
+        console.log(response)
+    }
+
     // function that let compare the input with the list of categories
 
-    return <>
-        <div className="container mx-auto flex justify-center items-center">
-            <Lottie animationData={student} />
-            <div className="w-1/2 space-y-4">
-                <h1>Skillekedin</h1>
-                <h2>Tired of failing your assessments skills on Linkedin ? </h2>
-                <p className="text-justify">Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.</p>
+    return (
+        <>
+            <div className="container mx-auto flex justify-center items-center">
+                <Lottie animationData={student} className='hidden md:block' />
+                <div className="mx-8 md:w-1/2 space-y-4">
+                    <h1>Skillekedin</h1>
+                    <h2>Tired of failing your assessments skills on Linkedin ? </h2>
+                    <p className="text-justify">Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.</p>
+                </div>
             </div>
-        </div>
-        <div className="bg-primary text-neutral">
-            <div className="container mx-auto flex justify-between items-center h-28">
-                <h1>Choose a quiz</h1>
-                <Box
-                    component="form"
-                    // sx={{
-                    //     '& > :not(style)': { m: 1, width: '25ch' },
-                    // }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField
-                        id="outlined-basic"
-                        variant="outlined"
-                        label="Search"
-                        onChange={searchHandler}
-                    />
-                </Box>
+            <div className="bg-primary text-neutral">
+                <div className="container mx-auto flex justify-between items-center h-28">
+                    <h1 className='ml-8'>Choose a quiz</h1>
+                    <Box
+                        component="form"
+                        sx={{
+                            '& > :not(style)': { m: 1, width: '150px' },
+                        }}
+                        noValidate
+                        autoComplete="off"
+                        className='mr-8'
+                    >
+                        <TextField
+                            id="outlined-basic"
+                            variant="outlined"
+                            label="Search"
+                            onChange={searchHandler}
+                        />
+                    </Box>
+                </div>
             </div>
-        </div>
-        <div>
-            <QuizChoice />
-        </div>
-    </>
+            <div>
+                <QuizChoice />
+            </div>
+        </>
+    )
 }
 
 export default HomePage;

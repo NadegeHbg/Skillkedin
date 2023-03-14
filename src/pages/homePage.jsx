5666d3c670b62931258f9d92856a899548de51a1
Kyubs1610 <diego.decort@gmail.com>
2023-03-14 13:04:54 +0100

    adding homepagecss for input

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import './homePage.css'
 import Lottie from "lottie-react";
 import { useState } from 'react';
 import textbl from '../assets/logos/Skillkedin-bluetext.png';
